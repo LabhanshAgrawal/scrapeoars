7645814cd3b49b0d46e568b9a50c01a51539258b
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2016-04-05 23:54:51 +0530

    Able to fetch list of courses

--- scrapeoars/spiders/scrapeoars_spider.py
+++ scrapeoars/spiders/scrapeoars_spider.py
@@ -4,11 +4,9 @@ class scrapeoarsSpider(scrapy.Spider):
     name = "scrapeoars"
     allowed_domains = ["http://172.26.142.75:4040/"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://172.26.142.75:4040/Common/CourseListing.asp"
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for sel in response.xpath('//html/body/center/table/tr[*]/td[1]/font/a/text()').extract():
+            print sel
