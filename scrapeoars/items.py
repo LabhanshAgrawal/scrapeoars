fa873140ac73dc2890c1609c5be649fe0b38d0ca
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2016-04-06 13:10:35 +0530

    Successfully scrapes all course data except current status

--- scrapeoars/items.py
+++ scrapeoars/items.py
@@ -11,14 +11,14 @@ import scrapy
 class ScrapeoarsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    course_no = scrapy.Field
-    title = scrapy.Field
-    instructor = scrapy.Field
-    instructor_mail = scrapy.Field
-    pre_req = scrapy.Field
-    credits = scrapy.Field
-    dept = scrapy.Field
-    schedule = scrapy.Field
-    instructor_notes = scrapy.Field
-    current_status = scrapy.Field
+    course_no = scrapy.Field()
+    title = scrapy.Field()
+    instructor = scrapy.Field()
+    instructor_mail = scrapy.Field()
+    pre_req = scrapy.Field()
+    credits = scrapy.Field()
+    dept = scrapy.Field()
+    schedule = scrapy.Field()
+    instructor_notes = scrapy.Field()
+    current_status = scrapy.Field()
 
--- scrapeoars/spiders/scrapeoars_spider.py
+++ scrapeoars/spiders/scrapeoars_spider.py
@@ -1,12 +1,31 @@
 import scrapy
+import re
+
+from scrapeoars.items import ScrapeoarsItem
 
 class scrapeoarsSpider(scrapy.Spider):
     name = "scrapeoars"
-    allowed_domains = ["http://172.26.142.75:4040/"]
+    allowed_domains = ["172.26.142.75"]
     start_urls = [
         "http://172.26.142.75:4040/Common/CourseListing.asp"
     ]
 
     def parse(self, response):
-        for sel in response.xpath('//html/body/center/table/tr[*]/td[1]/font/a/text()').extract():
-            print sel
+        for CourseName in response.xpath('//html/body/center/table/tr[*]/td[1]/font/a/text()').extract():
+            url = "http://172.26.142.75:4040/Utils/CourseInfoPopup2.asp?Course=" + CourseName
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
+        item = ScrapeoarsItem()
+        item['course_no'] = response.xpath('//html/body/table/tr[1]/td[2]/text()').extract()
+        item['title'] = response.xpath('//html/body/table/tr[2]/td[2]/text()').extract()
+        item['instructor'] = response.xpath('//html/body/table/tr[3]/td[2]/text()').extract()
+        item['instructor_mail'] = response.xpath('//html/body/table/tr[4]/td[2]/text()').extract()
+        item['pre_req'] = response.xpath('//html/body/table/tr[5]/td[2]/text()').extract()
+        item['credits'] = response.xpath('//html/body/table/tr[6]/td[2]/text()').extract()
+        item['dept'] = response.xpath('//html/body/table/tr[7]/td[2]/text()').extract()
+        text = ''.join(response.xpath('//html/body/table/tr[8]/td[2]/text()').extract())
+        item['schedule'] = str(re.sub(r"\s+", " ", text))
+        item['instructor_notes'] = response.xpath('//html/body/table/tr[9]/td[2]/text()').extract()
+        item['current_status'] = response.xpath('//html/body/table/tr[10]/td[2]/text()').extract()
+        yield item
